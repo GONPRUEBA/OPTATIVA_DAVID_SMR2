ba401398b4151de353ec021cddc6dc0cf57f4c07
GONZALO <gonzalo.martinezinfantes.25@campusfp.es>
2026-01-30 11:43:00 +0100

    .

---
+++ 2T/PLANTILLAcopiar/js/script.js
@@ -0,0 +1,50 @@
+// VERIFICAR POR CONSOLA BASICOS
+const navegador = document.getElementById('navegador');
+console.log(navegador);
+console.log(typeof navegador);
+console.log(navegador.innerHTML);
+console.log(navegador.innerText);
+console.log(navegador.tagName);
+
+const cajaImgBot = document.getElementsByClassName('cajaImgBot');
+console.log(cajaImgBot);
+console.log(cajaImgBot.length);
+console.log(cajaImgBot[1]);
+
+const link = document.getElementsByTagName('a');
+console.log(link);
+console.log(link[1].innerText);
+
+
+
+const rojo = document.querySelector('.botonROJO');
+console.log(rojo);
+
+const rojo2 = document.querySelectorAll('.botonROJO');
+console.log(rojo2);
+
+// CAMBIANDO COSITAS VISUALES
+const cuadroROJO = document.querySelectorAll('.cajaImgBot');
+cuadroROJO[1].style.backgroundColor = 'red'
+cuadroROJO[1].style.color = 'yellow'
+cuadroROJO[1].style.textTransform = 'uppercase'
+
+// const texto = document.getElementById('navegador');
+// texto.innerText = 'jugando';
+// texto.style.color = 'yellow';
+
+
+// function cambiarTexto(){
+    // texto.innerText = 'jugando';
+    // texto.style.color = 'yellow';
+// }
+
+// ELIMINAR ATRIBUTO
+// const eliminarATRIBUTO = document.getElementsByTagName('a');
+// console.log(link[1].getAttribute('href')); 
+// eliminarATRIBUTO[1].removeAttribute('href');
+
+// AÑADIR ATRIBUTO
+const añadirATRIBUTO = document.getElementsByTagName('a');
+console.log(link[1].setAttribute('href','campusfp.es')); 
+añadirATRIBUTO[1].setAttribute('href');
